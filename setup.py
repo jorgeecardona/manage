49deabbe75f6ae9d33524145342cc8f5223e74b7
Jorge Cardona <jorgeecardona@gmail.com>
2021-01-14 20:47:42 +0100

    Add new line

--- setup.py
+++ setup.py
@@ -4,21 +4,21 @@
 
 from setuptools import setup
 
-with open('README.rst') as readme_file:
-    readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
-    history = history_file.read()
+def long_description():
+    desc = []
+    for filename in ['README.rst', 'HISTORY.rst']:
+        with open(filename, 'r') as fd:
+            desc.append(fd.read())
 
-requirements = ['Click>=7.0', 'PyYAML>=3.11']
+    return '\n\n'.join(desc)
 
-test_requirements = ['pytest>=2.9.2', 'pytest-xdist>=1.14']
 
 setup(
     name='manage',
     version='0.1.14',
     description="Command Line Manager + Interactive Shell for Python Projects",
-    long_description=readme + '\n\n' + history,
+    long_description=long_description(),
     author="Bruno Rocha",
     author_email='rochacbruno@gmail.com',
     url='https://github.com/pthon-manage/manage',
@@ -26,7 +26,10 @@ setup(
     package_dir={'manage': 'manage'},
     entry_points={'console_scripts': ['manage=manage.cli:main']},
     include_package_data=True,
-    install_requires=requirements,
+    install_requires=[
+        'Click>=7.1',
+        'PyYAML>=5.3'
+    ],
     license="ISC license",
     zip_safe=False,
     keywords='manage',
@@ -35,12 +38,14 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: ISC License (ISCL)',
         'Natural Language :: English',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9'
     ],
     test_suite='tests',
-    tests_require=test_requirements,
+    tests_require=[
+        'pytest>=6.2',
+        'pytest-xdist>=2.2'
+    ],
 )
